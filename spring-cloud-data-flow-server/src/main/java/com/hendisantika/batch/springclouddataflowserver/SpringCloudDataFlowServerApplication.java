8fdea4a77626554e90715477d2a1d2c9696e6f51
Hendi Santika <hendisantika@gmail.com>
2018-12-09 17:04:06 +0700

    #7 - Add spring-cloud-data-flow-server module

---
+++ spring-cloud-data-flow-server/src/main/java/com/hendisantika/batch/springclouddataflowserver/SpringCloudDataFlowServerApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.batch.springclouddataflowserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudDataFlowServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudDataFlowServerApplication.class, args);
+    }
+}
