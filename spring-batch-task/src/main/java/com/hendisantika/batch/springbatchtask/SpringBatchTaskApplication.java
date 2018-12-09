337419821652791846f9480c9413b42571f30784
Hendi Santika <hendisantika@gmail.com>
2018-12-09 16:58:20 +0700

    #6 - Enable Task & set H2 DB - Spring Batch Task Module

--- spring-batch-task/src/main/java/com/hendisantika/batch/springbatchtask/SpringBatchTaskApplication.java
+++ spring-batch-task/src/main/java/com/hendisantika/batch/springbatchtask/SpringBatchTaskApplication.java
@@ -2,8 +2,10 @@ package com.hendisantika.batch.springbatchtask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.task.configuration.EnableTask;
 
 @SpringBootApplication
+@EnableTask
 public class SpringBatchTaskApplication {
 
 	public static void main(String[] args) {
