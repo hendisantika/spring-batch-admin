c23b5779a2a327e070054924fda9d364e989e2d4
Hendi Santika <hendisantika@gmail.com>
2018-12-09 17:33:57 +0700

    #16 - Add Test Case - spring batch task

--- spring-batch-task/src/test/java/com/hendisantika/batch/springbatchtask/SpringBatchTaskApplicationTests.java
+++ spring-batch-task/src/test/java/com/hendisantika/batch/springbatchtask/SpringBatchTaskApplicationTests.java
@@ -1,16 +1,49 @@
 package com.hendisantika.batch.springbatchtask;
 
+import com.hendisantika.batch.springbatchtask.job.CapitalizeNamesJobConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.batch.operations.NoSuchJobException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBatchTaskApplicationTests {
 
+	@Autowired
+	private JobLauncherTestUtils jobLauncherTestUtils;
+
 	@Test
-	public void contextLoads() {
+	public void testConvertNamesJob() throws Exception {
+		JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+		assertThat(jobExecution.getExitStatus().getExitCode()).isEqualTo("COMPLETED");
+	}
+
+	@Configuration
+	@Import(CapitalizeNamesJobConfig.class)
+	static class BatchTestConfig {
+
+		@Autowired
+		private Job capitalizeNamesJob;
+
+		@Bean
+		JobLauncherTestUtils jobLauncherTestUtils() throws NoSuchJobException {
+			JobLauncherTestUtils jobLauncherTestUtils = new JobLauncherTestUtils();
+			jobLauncherTestUtils.setJob(capitalizeNamesJob);
+
+			return jobLauncherTestUtils;
+		}
 	}
 
 }
