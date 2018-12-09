06d880c3e51174301c834044d0b162e69cc78db3
Hendi Santika <hendisantika@gmail.com>
2018-12-09 16:44:14 +0700

    #3 - Add Spring Batch Task Module

---
+++ spring-batch-task/src/main/java/com/hendisantika/batch/springbatchtask/SpringBatchTaskApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.batch.springbatchtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchTaskApplication.class, args);
+	}
+}
---
+++ spring-batch-task/src/test/java/com/hendisantika/batch/springbatchtask/SpringBatchTaskApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hendisantika.batch.springbatchtask;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBatchTaskApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
