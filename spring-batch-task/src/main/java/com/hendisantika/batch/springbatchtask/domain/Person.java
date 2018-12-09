58488f76da30d6fde6aa8ddb6f35ba9dcd82cf0e
Hendi Santika <hendisantika@gmail.com>
2018-12-09 17:19:06 +0700

    #11 - Add Person Domain - spring batch task

---
+++ spring-batch-task/src/main/java/com/hendisantika/batch/springbatchtask/domain/Person.java
@@ -0,0 +1,20 @@
+package com.hendisantika.batch.springbatchtask.domain;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-batch-admin
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2018-12-09
+ * Time: 17:18
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Data
+public class Person {
+    private String firstName;
+    private String lastName;
+}
