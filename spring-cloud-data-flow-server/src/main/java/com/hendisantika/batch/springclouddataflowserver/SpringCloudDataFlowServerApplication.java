dfc7f50aa2da706401cce498f2fd9ab7ea58f30d
Hendi Santika <hendisantika@gmail.com>
2018-12-09 17:13:01 +0700

    #9 - Add required libs - spring-cloud-data-flow-server module

--- spring-cloud-data-flow-server/src/main/java/com/hendisantika/batch/springclouddataflowserver/SpringCloudDataFlowServerApplication.java
+++ spring-cloud-data-flow-server/src/main/java/com/hendisantika/batch/springclouddataflowserver/SpringCloudDataFlowServerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDataFlowServer
 public class SpringCloudDataFlowServerApplication {
 
     public static void main(String[] args) {
